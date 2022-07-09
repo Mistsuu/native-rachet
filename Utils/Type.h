13328bf3ac645520a1961abd09cf7cf25f0ea51c
Mistsu <vietanhtran12@gmail.com>
2022-07-09 16:44:28 +0700

    Change git stage

--- Utils/Type.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#include <gmpxx.h>
-typedef mpz_class Int;
--- Utils/Utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Buffer.h"
-#include "Calc.h"
-#include "Const.h"
-#include "Random.h"
-#include "Type.h"
